16dc5a47b62d1478830e95bac11a21ffc396db56
SudattaDSA <138360776+SudattaDSA@users.noreply.github.com>
2024-03-03 11:44:55 +0530

    Time: 2 ms (99.79%), Space: 43.1 MB (60.77%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        HashSet<String> hs=new HashSet<>();
+        for(int i=0;i<paths.size();i++){
+            hs.add(paths.get(i).get(0));
+        }
+        for(int i=0;i<paths.size();i++){
+            if(!hs.contains(paths.get(i).get(1)))
+             return paths.get(i).get(1);
+        }
+        return "";
+    }
+}
